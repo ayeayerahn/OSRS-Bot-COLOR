ed008507f0fe2eb5e45ab337420a15b6f0587bb9
ayeayerahn <105690925+ayeayerahn@users.noreply.github.com>
2023-06-13 21:40:39 -0500

    Create chop_and_firemake

---
+++ src/model/osrs/AaronScripts/chop_and_firemake.py
@@ -0,0 +1,119 @@
+import time
+
+import pyautogui as pag
+import utilities.color as clr
+import utilities.api.item_ids as ids
+import utilities.random_util as rd
+import utilities.imagesearch as imsearch
+from model.osrs.osrs_bot import OSRSBot
+from utilities.api.morg_http_client import MorgHTTPSocket
+from utilities.api.status_socket import StatusSocket
+
+
+class OSRSchop_and_firemake(OSRSBot):
+    def __init__(self):
+        bot_title = "Chop and Firemake"
+        description = "This bot will light logs."
+        super().__init__(bot_title=bot_title, description=description)
+        # Set option variables below (initial value is only used during UI-less testing)
+        self.running_time = 360
+        self.options_set = True
+
+    def create_options(self):
+        self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
+
+    def save_options(self, options: dict):
+        for option in options:
+            if option == "running_time":
+                self.running_time = options[option]
+            else:
+                self.log_msg(f"Unknown option: {option}")
+                print("Developer: ensure that the option keys are correct, and that options are being unpacked correctly.")
+                self.options_set = False
+                return
+        self.log_msg(f"Running time: {self.running_time} minutes.")
+        self.log_msg("Options set successfully.")
+        self.options_set = True
+
+    def main_loop(self):
+
+        # Setup APIs
+        api_m = MorgHTTPSocket()
+        api_s = StatusSocket()
+
+        # Main loop
+        start_time = time.time()
+        end_time = self.running_time * 60
+        while time.time() - start_time < end_time:
+
+            self.woodcut()
+            while not api_m.get_is_player_idle():
+                time.sleep(1)
+            tile = self.get_nearest_tag(clr.CYAN)
+            self.mouse.move_to(tile.random_point())
+            self.mouse.click()
+            time.sleep(2)
+            while not api_m.get_is_player_idle():
+                time.sleep(1)
+            self.firemake(api_m)
+
+            self.update_progress((time.time() - start_time) / end_time)
+
+        self.update_progress(1)
+        self.log_msg("Finished.")
+        self.stop()
+
+    def woodcut(self):
+        tree = self.get_nearest_tag(clr.GREEN)
+        if tree:
+            self.mouse.move_to(tree.random_point())
+            self.mouse.click()
+            time.sleep(3)
+
+    def firemake(self, api_m: MorgHTTPSocket):
+        oak_logs = api_m.get_inv_item_indices(ids.logs)
+        for i in oak_logs:
+            oak_logs = api_m.get_inv_item_indices(ids.logs)
+            self.mouse.move_to(self.win.inventory_slots[0].random_point())
+            self.mouse.click()
+            self.mouse.move_to(self.win.inventory_slots[oak_logs[0]].random_point())
+            self.mouse.click()
+            api_m.wait_til_gained_xp("Firemaking", 15)
+        time.sleep(1)
+        self.log_msg("Out of logs. Time to cut again.")
+
+
+    def first_spot(self):
+        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.PINK)
+        self.log_msg("Heading to the first tile")
+        self.mouse.move_to(tile[0].random_point())
+        self.mouse.click()
+        time.sleep(5)
+
+    def open_bank(self):
+        banker = self.get_nearest_tag(clr.CYAN)
+        self.mouse.move_to(banker.random_point())  
+        self.mouse.click()
+        self.sleep(1,2)
+
+    def second_spot(self):
+        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.PURPLE)
+        self.log_msg("Heading to the second tile")
+        self.mouse.move_to(tile[0].random_point())
+        self.mouse.click()
+        time.sleep(3)
+
+    def sleep(self, num1, num2):
+        sleep_time = rd.fancy_normal_sample(num1, num2)   
+        #self.log_msg(f"Sleeping for {sleep_time} seconds")     #Uncomment this out if you wish to see how many seconds the sleep is doing
+        time.sleep(sleep_time)       
+
+    def third_spot(self):
+        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.GREEN)
+        self.log_msg("Heading to the third tile")
+        self.mouse.move_to(tile[0].random_point())
+        self.mouse.click()
+        time.sleep(5)
+
+# TO DO's
+# Maybe rotate through 3-4 different squares to add variation to "start point"
