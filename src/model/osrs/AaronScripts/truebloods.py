19284b62b20bc8849facac6c85aa7d30668aab32
ayeayerahn <105690925+ayeayerahn@users.noreply.github.com>
2023-06-13 19:49:01 -0500

    Update truebloods.py

--- src/model/osrs/AaronScripts/truebloods.py
+++ src/model/osrs/AaronScripts/truebloods.py
@@ -1,14 +1,11 @@
 import time
 
-import utilities.api.item_ids as ids
 import utilities.color as clr
-import utilities.random_util as rd
 import utilities.imagesearch as imsearch
 import utilities.ocr as ocr
 import pyautogui as pag
 from model.osrs.osrs_bot import OSRSBot
 from utilities.api.morg_http_client import MorgHTTPSocket
-from utilities.api.status_socket import StatusSocket
 
 
 class OSRStruebloods(OSRSBot):
@@ -21,23 +18,12 @@ class OSRStruebloods(OSRSBot):
         self.options_set = True
 
     def create_options(self):
-        """
-        Use the OptionsBuilder to define the options for the bot. For each function call below,
-        we define the type of option we want to create, its key, a label for the option that the user will
-        see, and the possible values the user can select. The key is used in the save_options function to
-        unpack the dictionary of options after the user has selected them.
-        """
         self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
         self.options_builder.add_text_edit_option("text_edit_example", "Text Edit Example", "Placeholder text here")
         self.options_builder.add_checkbox_option("multi_select_example", "Multi-select Example", ["A", "B", "C"])
         self.options_builder.add_dropdown_option("menu_example", "Menu Example", ["A", "B", "C"])
 
     def save_options(self, options: dict):
-        """
-        For each option in the dictionary, if it is an expected option, save the value as a property of the bot.
-        If any unexpected options are found, log a warning. If an option is missing, set the options_set flag to
-        False.
-        """
         for option in options:
             if option == "running_time":
                 self.running_time = options[option]
@@ -57,20 +43,6 @@ class OSRStruebloods(OSRSBot):
         self.options_set = True
 
     def main_loop(self):
-        """
-        When implementing this function, you have the following responsibilities:
-        1. If you need to halt the bot from within this function, call `self.stop()`. You'll want to do this
-           when the bot has made a mistake, gets stuck, or a condition is met that requires the bot to stop.
-        2. Frequently call self.update_progress() and self.log_msg() to send information to the UI.
-        3. At the end of the main loop, make sure to call `self.stop()`.
-
-        Additional notes:
-        - Make use of Bot/RuneLiteBot member functions. There are many functions to simplify various actions.
-          Visit the Wiki for more.
-        - Using the available APIs is highly recommended. Some of all of the API tools may be unavailable for
-          select private servers. For usage, uncomment the `api_m` and/or `api_s` lines below, and use the `.`
-          operator to access their functions.
-        """
         # Setup APIs
         api_m = MorgHTTPSocket()
         # api_s = StatusSocket()
@@ -85,8 +57,6 @@ class OSRStruebloods(OSRSBot):
         # 1a. Open bank
             self.open_bank()
             time.sleep(4)
-            # while not api_m.get_is_player_idle():
-            #     time.sleep(1)
         # 1b. Retrieve supplies
             self.get_supplies(api_m)
             time.sleep(0.5)
@@ -116,7 +86,6 @@ class OSRStruebloods(OSRSBot):
 
 
     def get_supplies(self, api_m:MorgHTTPSocket):
-    # add a check if the blood essence is in the inventory
         # Assumes pouches, active blood ess, bank tabs, and rune pouch are already in inventory
         # Assumes withdraw all is defaulted
         self.deposit_all()
@@ -202,7 +171,6 @@ class OSRStruebloods(OSRSBot):
             self.mouse.move_to(altar.random_point(), mouseSpeed='fast')
             self.mouse.click()
         api_m.wait_til_gained_xp("Runecraft", 1)
-        #time.sleep(1.5) # Gives enough time to let your character become idle
 
     def empty_pouches(self):
         pag.keyDown('shift')
@@ -232,7 +200,6 @@ class OSRStruebloods(OSRSBot):
         fairy_ring = self.get_nearest_tag(clr.YELLOW)
         self.mouse.move_to(fairy_ring.random_point(), mouseSpeed='fast')
         self.mouse.click()
-        #time.sleep(3)
 
     def deposit_all(self): 
         deposit_img = imsearch.BOT_IMAGES.joinpath("Aarons_images", "deposit.png") 
@@ -264,23 +231,9 @@ class OSRStruebloods(OSRSBot):
             pag.press('2')
             time.sleep(1)
             pag.press('space')
-            # time.sleep(1)
-            # pag.press('2')
-            # time.sleep(1)
-            # pag.press('space')
-            # time.sleep(1)
-            # pag.press('space')
-            # time.sleep(1)
-            # pag.press('2')
-            # time.sleep(1)
-            # pag.press('space')
-            # time.sleep(1)
-            # pag.press('space')
-            # time.sleep(1)
             self.mouse.move_to(self.win.cp_tabs[3].random_point())
             self.mouse.click()
             time.sleep(1)
-            #self.open_bank()
 
     def check_for_blood_ess(self):
         blood_ess_inv_active_img = imsearch.BOT_IMAGES.joinpath("Aarons_images", "Blood_essence_active_inv.png")
