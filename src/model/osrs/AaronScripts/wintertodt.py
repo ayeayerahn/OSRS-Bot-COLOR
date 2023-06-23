5f11e1022e29020196df003d35981e6d445ed3c4
ayeayerahn <105690925+ayeayerahn@users.noreply.github.com>
2023-06-23 14:31:39 -0500

    a

--- src/model/osrs/AaronScripts/wintertodt.py
+++ src/model/osrs/AaronScripts/wintertodt.py
@@ -1,5 +1,4 @@
 import time
-import random
 
 import pyautogui as pag
 import utilities.color as clr
@@ -47,18 +46,12 @@ class OSRSwintertodt(OSRSBot):
         while time.time() - start_time < end_time:
 
             # 1. Open bank
-            #self.open_bank()
             # 2. Withdraw supplies
-            #self.withdraw_supplies(api_m)
             # 3. Walk to first spot
-            #self.first_spot()
             # 4. Click on big door
-            #self.big_door()
             # 5. Walk to second spot
-            #self.second_spot()
             # 6. Begin wintertodt
-            self.wintertodt(api_m)
-            # 7. Open bank (#1)
+            # 7. Open bank
 
             self.update_progress((time.time() - start_time) / end_time)
 
@@ -67,69 +60,5 @@ class OSRSwintertodt(OSRSBot):
         self.stop()
 
 
-    def first_spot(self):
-        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.YELLOW)
-        self.log_msg("Heading to the first tile")
-        self.mouse.move_to(tile[0].random_point())
-        self.mouse.click()
-        time.sleep(5)
-
-    def big_door(self):
-        door = self.get_nearest_tag(clr.CYAN)
-        self.mouse.move_to(door.random_point())  
-        self.mouse.click()
-        self.sleep(5,7)        
-
-    def open_bank(self):
-        banker = self.get_nearest_tag(clr.CYAN)
-        self.mouse.move_to(banker.random_point())  
-        self.mouse.click()
-        self.sleep(1,2)
-
-    def second_spot(self):
-        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.ORANGE)
-        self.log_msg("Heading to the second tile")
-        self.mouse.move_to(tile[0].random_point())
-        self.mouse.click()
-        self.sleep(4,6)
-
-    def sleep(self, num1, num2):
-        sleep_time = rd.fancy_normal_sample(num1, num2)   
-        #self.log_msg(f"Sleeping for {sleep_time} seconds")     #Uncomment this out if you wish to see how many seconds the sleep is doing
-        time.sleep(sleep_time)    
-
-    def third_spot(self):
-        tile = self.get_all_tagged_in_rect(self.win.game_view, clr.GREEN)
-        self.log_msg("Heading to the third tile")
-        self.mouse.move_to(tile[0].random_point())
-        self.mouse.click()
-        time.sleep(5)
-
-    def withdraw_supplies(self, api_m: MorgHTTPSocket):
-        logs_bank_img = imsearch.BOT_IMAGES.joinpath("Aarons_images", "Willow_logs_bank.png")
-        while True:
-            if logs := imsearch.search_img_in_rect(logs_bank_img, self.win.game_view):
-                self.mouse.move_to(logs.random_point())
-                self.mouse.click()
-                time.sleep(1)
-            if api_m.get_is_inv_full():
-                pag.press('escape')
-                time.sleep(0.5)
-                break
-    
-    def wintertodt(self, api_m: MorgHTTPSocket):
-        tree = self.get_nearest_tag(clr.CYAN)
-        fire = self.get_nearest_tag(clr.GREEN)
-        brazier = self.get_nearest_tag(clr.YELLOW)
-        while True:
-            # If inventory is not full and player is idle
-            while not api_m.get_is_inv_full() and api_m.get_is_player_idle():
-                self.mouse.move_to(tree.random_point())  
-                self.mouse.click()
-                self.sleep(1,2)
-            if api_m.get_is_inv_full():
-                self.mouse.move_to(fire.random_point())  
-                self.mouse.click()
-                self.sleep(5,7)
-
-# TO DO's
+    def gather_bruma(self):
+        
